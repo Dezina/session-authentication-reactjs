6b39c9d5e2f20d0417c401bec600827ef5a2e37a
dezina pereira <dezinapereira1@gmail.com>
2023-04-09 18:55:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Page1 from "./components/Page1";
+import Page2 from "./components/Page2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <ToastContainer></ToastContainer>
+    
+      <Switch>
+        <Route exact path="/" component={Page1} />
+        <Route exact path="/Page2" component={Page2} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
